5fd36055314969a858c5aba1fd87cd58057e69a8
Tahmid Ahmed <tahmid@midhat.io>
2024-06-25 18:28:35 -0700

    Added some motion to the scrollbar, reacts to velocity / 0.1 changes in scroll and applies it to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,43 +6,62 @@ import { stat } from "fs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from "framer-motion";
 
 export function Navbar() {
   const path = usePathStore((state) => state.path);
   const setPath = usePathStore((state) => state.setPath);
+
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+
+  const scaledVelocity = useTransform(scrollVelocity, (v) => v / 10);
+
+  const yTransform = useSpring(scaledVelocity, {
+    stiffness: 30,
+    damping: 25,
+    restDelta: 0.001,
+  });
   return (
     <>
-      <nav className="sticky top-10 z-10 bg-zinc-900/50 backdrop-filter backdrop-blur-md border-1 rounded-full w-3/4 lg:w-1/2 border-gray-800 justify-self-center m-auto">
-        <div className="max-w-5xl mx-auto px-4">
-          <div className="flex items-center justify-between h-16 mx-3 space-x-4">
-            <a
-              href="#"
-              className={`${
-                path === "/" ? "text-white" : "text-gray-700"
-              } hover:drop-shadow-2xl`}
-              onClick={(e) => {
-                e.preventDefault();
-                setPath("/");
-              }}
-            >
-              Home
-            </a>
-            <Progress className="rounded-full" />
-            <a
-              href="#projects"
-              className={`${
-                path === "/projects" ? "text-white" : "text-gray-700"
-              } hover:drop-shadow-2xl`}
-              onClick={(e) => {
-                e.preventDefault();
-                setPath("/projects");
-              }}
-            >
-              Projects
-            </a>
-          </div>
+      <motion.nav
+        className="sticky top-10 z-10 bg-zinc-900/50 backdrop-filter backdrop-blur-md border-1 rounded-full w-3/4 lg:w-1/2 border-gray-800 justify-self-center m-auto"
+        style={{ y: yTransform }}
+      >
+        <div className="flex items-center justify-between h-16 mx-3 space-x-4">
+          <a
+            href="#"
+            className={`${
+              path === "/" ? "text-white" : "text-gray-700"
+            } hover:drop-shadow-2xl`}
+            onClick={(e) => {
+              e.preventDefault();
+              setPath("/");
+            }}
+          >
+            Home
+          </a>
+          <Progress className="rounded-full" />
+          <a
+            href="#projects"
+            className={`${
+              path === "/projects" ? "text-white" : "text-gray-700"
+            } hover:drop-shadow-2xl`}
+            onClick={(e) => {
+              e.preventDefault();
+              setPath("/projects");
+            }}
+          >
+            Projects
+          </a>
         </div>
-      </nav>
+      </motion.nav>
     </>
   );
 }
