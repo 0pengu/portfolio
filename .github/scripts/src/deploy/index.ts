17818a85883c0dc18b2ac231662eac72c92ad9b7
Tahmid Ahmed <tahmid@midhat.io>
2026-08-23 05:56:42 -0400

    fix ci env (#8)

--- .github/scripts/src/deploy/index.ts
+++ .github/scripts/src/deploy/index.ts
@@ -33,7 +33,7 @@ function parseCiEnv(ciEnv: Record<string, string | undefined>) {
   const githubAppAppId = (() => {
     const v = ciEnv["_GITHUB_APP_APP_ID"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_APP_ID from .env.ci");
+      throw new Error("Missing _GITHUB_APP_APP_ID from .env.ci");
     }
     return v;
   })();
@@ -41,15 +41,15 @@ function parseCiEnv(ciEnv: Record<string, string | undefined>) {
   const githubAppInstallationId = (() => {
     const v = ciEnv["_GITHUB_APP_INSTALLATION_ID"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_INSTALLATION_ID from .env.ci");
+      throw new Error("Missing _GITHUB_APP_INSTALLATION_ID from .env.ci");
     }
     return v;
   })();
 
   const githubAppPemContent = (() => {
-    const v = ciEnv["GITHUB_APP_PEM_CONTENT"];
+    const v = ciEnv["_GITHUB_APP_PEM_CONTENT"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_PEM_CONTENT from .env.ci");
+      throw new Error("Missing _GITHUB_APP_PEM_CONTENT from .env.ci");
     }
     return v;
   })();
